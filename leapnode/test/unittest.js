12d40d745ef065814b9be83b1dfaec2cd6d455dd
ramyamariappan <ramya.mariappan@sjsu.edu>
2016-12-05 00:11:28 -0800

    testing hooks

--- leapnode/test/unittest.js
+++ leapnode/test/unittest.js
@@ -3,7 +3,6 @@ var should = require("should");
 var server = supertest.agent("http://localhost:8000");
 
 describe("SAMPLE unit test",function(){
-
   // #1 should return home page
   it("should return home page",function(done){
     // calling home page
