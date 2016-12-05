f7090e0e7066bf028d8bde5996181e33d013a68c
ramyamariappan <ramya.mariappan@sjsu.edu>
2016-12-05 00:44:15 -0800

    Update unittest.js

--- leapnode/test/unittest.js
+++ leapnode/test/unittest.js
@@ -3,18 +3,17 @@ var should = require("should");
 var server = supertest.agent("http://localhost:8000");
 
 describe("SAMPLE unit test",function(){
+
   // #1 should return home page
   it("should return home page",function(done){
     // calling home page
     server
     .get("/")
     .expect("Content-type",/text/)
-    .expect(200) // THis is HTTP response
-    .end(function(err,res){
-      // HTTP status should be 200
-      res.status.should.equal(200);
-      done();
-    });
+    .expect(function(err,res){
+        return false;      
+    })
+    .expect(200, done);
   });
 
 });
